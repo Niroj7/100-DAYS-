0db78f7d5f65548e9a6a52e1ef0924f1fdc4340d
Niroj7 <nirojkoirala7@gmail.com>
2025-07-01 22:13:19 -0400

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,82 @@
+from operator import truediv
+from subprocess import list2cmdline
+
+""""if __name__ == '__main__':
+    print('hello world')
+thislist= ["apple", "banana", "cherry"]
+print (thislist[-2])
+print (thislist[-1])
+
+list1 = ["abc", 34,True, 40,"male"]
+print (list1[0:3])
+print (list1[1:3])
+print (list1[:3])
+print (list1[1:])
+print (list1[3:-1])
+
+#max
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+
+print(thisdict)
+print(thisdict["model"])
+print(thisdict .keys())
+print(thisdict.values())
+if "name" in thisdict:
+    print(True)
+
+    #List2=[1,3,2,5,6,2]
+   # dic2= {
+     #-   3:1,
+      #  1:1,
+      # # 2:2,
+       # 5:1,
+        #6:1
+   #  }-#
+#target=1
+#for x in List2:
+#if x == target:
+     #count= count+1
+
+        # print(count)
+
+#for y in list1 :
+    """""""""
+        """continue""""""
+        print(w)"""
+"""list2 =[1,3,2,5,6,2,3,3,1,2,1,2,2]
+dist6={
+
+}
+print(dist6)
+for j in list2:
+    if j in dict6:
+        dist6[j] = dist6[j]+ 1
+        
+        maxm=0
+        for k in list2:
+          value= list2[k]
+            if value > maxm:
+                maxm = value"""""
+
+
+
+
+
+
+
+
+print(helloworld)
+
+
+
+
+
+
+
+
+
+
