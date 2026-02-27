3c56d1a94457f8763d33c2c8860f79f33a7077dc
Niroj koirala <nirojkoirala7@gmail.com>
2026-02-26 21:45:46 -0500

    Update JSON EXT 2.py

--- JSON EXT 2.py
+++ JSON EXT 2.py
@@ -5,7 +5,7 @@ The program uses Python’s built-in urllib and json libraries to:
 
 Send a request to the API with the given location
 .
-Parse the returned JS
+Parse the returned Js
 
 Pull out the plus_code from the first result
 
